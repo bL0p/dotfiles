48478903b6d1f23bbb1c74665d92f5ee51cc3b22
Bernardo Oliveira Pinto <bernardo.pinto@byclosure.com>
2015-11-03 18:42:42 +0000

    Add pbcopy and pbpaste to linux

--- system/alias.zsh
+++ system/alias.zsh
@@ -2,3 +2,8 @@
 alias c="clear"
 alias e="exit"
 alias dev='cd $HOME/Documents/workspace'
+
+if [[ `uname` == 'Linux' ]]; then
+  alias pbcopy='xclip -selection clipboard'
+  alias pbpaste='xclip -selection clipboard -o'
+fi
