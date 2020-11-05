1edb6b5f5751e975dce9b6fe504cd22ac6aa051c
Bernardo Oliveira Pinto <>
2020-11-05 08:51:19 +0000

    Updated configurations

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -1,2 +1,3 @@
 # Load rvm
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+export PATH="$PATH:$HOME/.rvm/bin"
--- system/alias.zsh
+++ system/alias.zsh
@@ -1,7 +1,6 @@
 # Quick commands
 alias c="clear"
 alias e="exit"
-alias dev='cd $HOME/workspace/easy-easy-apps'
 
 if [[ `uname` == 'Linux' ]]; then
   alias pbcopy='xclip -selection clipboard'
