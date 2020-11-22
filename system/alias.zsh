996b9a884c52cd3b6ce40016a967fa1bef881fda
Bernardo Oliveira Pinto <>
2020-11-22 10:52:03 +0000

    Add command to get my public IP (v4)

--- system/alias.zsh
+++ system/alias.zsh
@@ -2,7 +2,10 @@
 alias c="clear"
 alias e="exit"
 
+alias myip="dig @ns1.google.com TXT o-o.myaddr.l.google.com +short -4"
+
 if [[ `uname` == 'Linux' ]]; then
   alias pbcopy='xclip -selection clipboard'
   alias pbpaste='xclip -selection clipboard -o'
 fi
+
