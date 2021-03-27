bb75d767a0ff4ae71833ca728bd65385503e22e3
Bernardo Oliveira Pinto <>
2021-03-27 18:21:53 +0000

    Updates
    - install command adapted for an empty system
    - add software install script for ubuntu
    - update oh-my-zsh version

---
+++ ubuntu.install_software.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y zsh fzf openjdk-8-jdk openjdk-11-jdk php
+
+git clone https://github.com/jenv/jenv.git ~/.jenv
+jenv add /usr/lib/jvm/java-1.8.0-openjdk-amd64
+jenv add /usr/lib/jvm/java-11-openjdk-amd64
+jenv enable-plugin export
+jenv global 1.8
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
