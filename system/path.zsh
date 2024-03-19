36e8142f73f74bf8a500c94c20c33a5ca927de91
Bernardo Oliveira Pinto <bernardoopinto@gmail.com>
2024-03-19 12:44:40 +0000

    Update configs

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/bin:/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
